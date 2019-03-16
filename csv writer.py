3c66cf590dd38b59eb499917a0959d996f81b686
aburmals <48114243+aburmals@users.noreply.github.com>
2019-03-17 02:36:00 +0530

    Update csv writer.py

--- csv writer.py
+++ csv writer.py
@@ -3,11 +3,12 @@ import csv
 import random
 s=1
 for i in range(1,10):
-    x=open(f"file_0{i}.csv",'x')
-    y=[["#","num1","num2","num3","num4","num5"]]
+    x=open(f"file_0{i}.csv",'x') #creates new .csv file
+    y=[["#","num1","num2","num3","num4","num5"]] #initialising for the header
     while os.stat(f"file_0{i}.csv").st_size<2000:
         y.append([s,random.randint(1,500),random.randint(1,500),random.randint(1,500),random.randint(1,500),random.randint(1,500),random.randint(1,500)])
+        #to create the values to append to the .csv file at corresponding columns
         s=s+1
-        with open (f"file_0{i}.csv",'w') as randwrite:
+        with open (f"file_0{i}.csv",'w') as randwrite: #to write the .csv file
             writer = csv.writer(randwrite)
-            writer.writerow(y)
+            writer.writerow(y) #appends the values to the cooresponding columns
